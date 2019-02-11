559cadb5adc3af3d74f3069918f9f066987bc4e3
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2019-02-11 18:33:48 +0200

    fix: Also remove comments

--- js/util/minify.js
+++ js/util/minify.js
@@ -1,5 +1,5 @@
 import { transform } from '@babel/standalone';
 
 export default function minify(code) {
-  return transform(code, { presets: ['minify'] }).code;
+  return transform(code, { presets: ['minify'], comments: false }).code;
 }
