ede698935446c0e503ba1f2cea288bbcd81c7279
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2019-02-11 22:49:29 +0200

    fix: Disable builtIns plugin for minifier, closes #40

--- js/util/minify.js
+++ js/util/minify.js
@@ -1,5 +1,7 @@
 import { transform } from '@babel/standalone';
 
 export default function minify(code) {
-  return transform(code, { presets: ['minify'], comments: false }).code;
+  // "builtins: false" fixes https://github.com/circlecell/jscompress.com/issues/40
+  // TODO enable it back as soon as
+  return transform(code, { presets: [['minify', { builtIns: false }]], comments: false }).code;
 }
